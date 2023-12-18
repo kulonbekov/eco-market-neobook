412b48569a97e0f8913cb0c74cb7d9e17e9d489b
Kulonbekov <kulonbekov@gmail.com>
2023-12-18 11:40:12 +0600

    Add service CategoryService and method getAll()

---
+++ src/main/java/kg/taalai/ecomarket/models/service/CategoryService.java
@@ -0,0 +1,10 @@
+package kg.taalai.ecomarket.models.service;
+
+import kg.taalai.ecomarket.models.dtos.CategoryDto;
+
+import java.util.List;
+
+public interface CategoryService {
+
+    List<CategoryDto> getAll();
+}
