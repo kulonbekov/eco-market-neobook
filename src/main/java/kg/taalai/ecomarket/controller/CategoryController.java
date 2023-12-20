c148376d088300ffcb557312f485d294bb00a5d6
Kulonbekov <kulonbekov@gmail.com>
2023-12-20 18:10:24 +0600

    add CategoryController with one method "getAll()"

---
+++ src/main/java/kg/taalai/ecomarket/controller/CategoryController.java
@@ -0,0 +1,25 @@
+package kg.taalai.ecomarket.controller;
+
+import kg.taalai.ecomarket.models.dtos.CategoryDto;
+import kg.taalai.ecomarket.models.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.swing.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/category")
+@RequiredArgsConstructor
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @GetMapping("/get/all")
+    ResponseEntity<List<CategoryDto>> findAllByCategory(){
+        return ResponseEntity.ok(categoryService.getAll());
+    }
+}
