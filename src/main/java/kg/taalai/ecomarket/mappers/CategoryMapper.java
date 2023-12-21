c1276631580a117a7eff54a92f37209ab3071936
Kulonbekov <kulonbekov@gmail.com>
2023-12-21 17:57:08 +0600

    add annotation @Mapper

--- src/main/java/kg/taalai/ecomarket/mappers/CategoryMapper.java
+++ src/main/java/kg/taalai/ecomarket/mappers/CategoryMapper.java
@@ -2,8 +2,10 @@ package kg.taalai.ecomarket.mappers;
 
 import kg.taalai.ecomarket.models.dtos.CategoryDto;
 import kg.taalai.ecomarket.models.entities.Category;
+import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
+@Mapper
 public interface CategoryMapper extends BaseMapper<Category, CategoryDto> {
 
     CategoryMapper INSTANCE = Mappers.getMapper(CategoryMapper.class);
