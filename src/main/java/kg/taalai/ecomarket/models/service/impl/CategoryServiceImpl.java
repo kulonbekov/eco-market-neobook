6a0d941f6a4abaeeaa0d27811c3404e04b94c33c
Kulonbekov <kulonbekov@gmail.com>
2023-12-21 17:59:26 +0600

    I added a method to the category service that initializes the database with random data

--- src/main/java/kg/taalai/ecomarket/models/service/impl/CategoryServiceImpl.java
+++ src/main/java/kg/taalai/ecomarket/models/service/impl/CategoryServiceImpl.java
@@ -1,5 +1,7 @@
 package kg.taalai.ecomarket.models.service.impl;
 
+import com.github.javafaker.Faker;
+import jakarta.annotation.PostConstruct;
 import kg.taalai.ecomarket.mappers.CategoryMapper;
 import kg.taalai.ecomarket.models.dtos.CategoryDto;
 import kg.taalai.ecomarket.models.entities.Category;
@@ -8,7 +10,10 @@ import kg.taalai.ecomarket.repository.CategoryRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
@@ -17,6 +22,31 @@ public class CategoryServiceImpl implements CategoryService {
 
     private final CategoryRepo categoryRepo;
     CategoryMapper categoryMapper = CategoryMapper.INSTANCE;
+
+
+    private List<Category> categories;
+
+    @PostConstruct
+    public void loadCategoryInDB() {
+        Faker faker = new Faker();
+        Set<String> uniqueCategoryNames = new HashSet<>();
+        categories = new ArrayList<>();
+
+        while (uniqueCategoryNames.size() < 10) {
+            String categoryName = faker.commerce().department();
+            if (uniqueCategoryNames.add(categoryName)) {
+                Category category = Category.builder()
+                        .name(categoryName)
+                        .imageUrl(faker.company().url())
+                        .build();
+                categories.add(category);
+            }
+        }
+
+        if (categoryRepo.findAll().isEmpty()) {
+            categoryRepo.saveAll(categories);
+        }
+    }
 
     @Override
     public List<CategoryDto> getAll() {
