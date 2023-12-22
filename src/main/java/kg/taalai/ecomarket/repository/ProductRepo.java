dd27fe8b38b9ddb2eaa71b429e0a53a1276c0bf3
Kulonbekov <kulonbekov@gmail.com>
2023-12-22 16:58:53 +0600

    in repo add method "findDistinctByCategoriesIdIn"

--- src/main/java/kg/taalai/ecomarket/repository/ProductRepo.java
+++ src/main/java/kg/taalai/ecomarket/repository/ProductRepo.java
@@ -4,6 +4,10 @@ import kg.taalai.ecomarket.models.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepo extends JpaRepository<Product, Long> {
+
+    List<Product> findDistinctByCategoriesIdIn(List<Long> ids);
 }
