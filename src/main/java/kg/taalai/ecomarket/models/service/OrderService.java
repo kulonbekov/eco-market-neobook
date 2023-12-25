d3a684b683705536e10e43086ad486733afdcec1
Kulonbekov <kulonbekov@gmail.com>
2023-12-25 12:35:08 +0600

    add methods 1. Add method "save" for Order 2. Add method "findByUser" for Order

--- src/main/java/kg/taalai/ecomarket/models/service/OrderService.java
+++ src/main/java/kg/taalai/ecomarket/models/service/OrderService.java
@@ -3,7 +3,10 @@ package kg.taalai.ecomarket.models.service;
 import kg.taalai.ecomarket.models.dtos.OrderDto;
 import kg.taalai.ecomarket.models.entities.Order;
 
+import java.util.List;
+
 public interface OrderService {
 
     OrderDto save(OrderDto orderDto);
+    List<OrderDto> findByUser (Long id);
 }
